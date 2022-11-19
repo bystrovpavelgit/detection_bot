4cac8042b005b8722b96f5daf29eda507b2e5752
bystrovpavelgit <bystrov.pavelme@gmail.com>
2022-11-19 10:40:02 +0300

    stat views

---
+++ webapp/stat/views.py
@@ -0,0 +1,18 @@
+"""blueprint for statistics"""
+import os
+from flask import render_template, Blueprint
+from flask_login import login_required
+from object_detection.charts import get_chart
+from webapp.business_logic import get_stats
+
+blueprint = Blueprint("stat", __name__, url_prefix="/stat")
+
+
+@blueprint.route("/show")
+@login_required
+def show_stat():
+    """show_stat endpoint"""
+    answers = get_stats()
+    text = answers[0]
+    chart_img = "/" + get_chart(os.path.join("webapp", "static"), answers[1], answers[2])
+    return render_template("stat/show_stat.html", answers=text, chart_img=chart_img)
