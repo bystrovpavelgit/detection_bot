5803b274d2d5f25091f4a6f80d2f0dcf8e83b1f4
bystrovpavelgit <bystrov.pavelme@gmail.com>
2022-12-06 22:40:20 +0300

    fixed formatting

--- webapp/business_logic.py
+++ webapp/business_logic.py
@@ -3,10 +3,8 @@ from object_detection.processing import process_picture
 from object_detection.cars_counting import detect_all_autos
 from sqlalchemy.sql import func
 from webapp.user.models import Defects, CarCounts
-from webapp.db import DB as db
+from webapp.db import DB
 from webapp.dl import CARS_RCNN_MODEL, DEFECTS_MODEL, LABEL_ENCODER
-
-
 
 
 def detect(filename):
@@ -14,8 +12,8 @@ def detect(filename):
     print("Ищем дефекты на " + filename)
     result, y_pred = process_picture(DEFECTS_MODEL, LABEL_ENCODER, filename)
     row = Defects(image=filename, object_class=int(y_pred), object_label=result)
-    db.session.add(row)
-    db.session.commit()
+    DB.session.add(row)
+    DB.session.commit()
     return result
 
 
@@ -29,16 +27,16 @@ def get_stats():
     total = tentity.total
     query_count = CarCounts.query.count()
     messages = [f"всего найдено машин: {total} по {query_count} запросам",
-        f"изображений с асфальтом: {asphalt_count}",
-        f"изображений с дефектом: {defects_count}",
-        f"изображений с посторонним предметом {other_count}"]
-    return (messages, [asphalt_count, defects_count, other_count], labels)
+                f"изображений с асфальтом: {asphalt_count}",
+                f"изображений с дефектом: {defects_count}",
+                f"изображений с посторонним предметом {other_count}"]
+    return messages, [asphalt_count, defects_count, other_count], labels
 
 
 def car_count(filename):
     """car count"""
     result = detect_all_autos(CARS_RCNN_MODEL, filename)
     row = CarCounts(image=filename, car_count=result[0], ratio=0.0)
-    db.session.add(row)
-    db.session.commit()
+    DB.session.add(row)
+    DB.session.commit()
     return result[1:]
